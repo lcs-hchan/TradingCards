4463c04394e383e067ea629b96ae3bdd0b48a229
Hayden Chan <hchan@lcs.on.ca>
2024-01-26 19:28:14 -0500

    Able to use list to make code smaller

--- TradingCards/view/ContentView.swift
+++ TradingCards/view/ContentView.swift
@@ -10,52 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack{
-            VStack(alignment: .leading){
-                List{
-                    NavigationLink{
-                        TemplateView(character: obilsek)
-                    }label: {
-                        Text("Obelisk The Tormentor")
-                            .font(
-                                .custom(
-                                    "AmericanTypewriter", size: 20,
-                                    relativeTo: .body  )
-                            )
-
-                    }
-                    NavigationLink{
-                        TemplateView(character: ra)
-                    }label: {
-                        Text("The Winged Dragon Of Ra")
-                            .font(
-                                .custom(
-                                    "AmericanTypewriter", size: 20,
-                                    relativeTo: .body  )
-                            )
-
-                    }
-                    NavigationLink{
-                        TemplateView(character: stlifer)
-                    }label: {
-                        Text("Slifer The Sky Dragon")
-                            .font(
-                                .custom(
-                                    "AmericanTypewriter", size: 20,
-                                    relativeTo: .body  )
-                            )
-
-                    }
+            
+            List(allCards){ currentCard in
+                NavigationLink{
+                    TemplateView(character: currentCard)
+                }label: {
+                    Text(currentCard.name)
+                        .font(
+                            .custom(
+                                "AmericanTypewriter", size: 20,
+                                relativeTo: .body  )
+                        )
                 }
+                
             }
+            
             .listStyle(.plain)
             .background(.blue)
             .navigationTitle("Yugioh God Cards")
-           
+    }
         }
        
     }
         
-}
+
 
 #Preview {
     ContentView()
